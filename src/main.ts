dbd3b580021d81a1abc626f428265244bc3cc9b8
HanHyunsoo <gustn4563@gmail.com>
2023-07-31 18:21:54 +0900

    fix: Fix ci cd

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const serviceAccount = require('../firebase-adminsdk.json');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
   });
 }
 bootstrap();
